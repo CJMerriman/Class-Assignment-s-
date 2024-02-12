da2faedb1f1b0592c96a51bc06d1f57f468ac51c
CJMerriman <148406377+CJMerriman@users.noreply.github.com>
2024-02-12 02:28:01 -0500

    main.js

--- Assignments/Week 4/fizzy/main.js
+++ Assignments/Week 4/fizzy/main.js
@@ -1,6 +1,48 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+// Define the fizzBuzz function
+function fizzBuzz(n) {
+  let result = [];
+  for (let i = 1; i <= n; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+          result.push("FizzBuzz");
+      } else if (i % 3 === 0) {
+          result.push("Fizz");
+      } else if (i % 5 === 0) {
+          result.push("Buzz");
+      } else {
+          result.push(i.toString());
+      }
+  }
+  return result;
+}
+
+// Call the fizzBuzz function with an argument (e.g., 15)
+//
+//console.log(fizzBuzz(15));
+function fizzy(number) {
+  if (number % 3 === 0 && number % 5 !== 0) {
+      return 'Fizz';
+  } else if (number % 5 === 0 && number % 3 !== 0) {
+      return 'Buzz';
+  } else if (number % 3 === 0 && number % 5 === 0) {
+      return 'FizzBuzz';
+  } else {
+      return number;
+  }
+}
+function fizzy(number) {
+  if (number % 3 === 0 && number % 5 !== 0) {
+      return 'Fizz';
+  } else if (number % 5 === 0 && number % 3 !== 0) {
+      return 'Buzz';
+  } else if (number % 3 === 0 && number % 5 === 0) {
+      return 'FizzBuzz';
+  } else {
+      return number;
+  }
+}
 
 
 
