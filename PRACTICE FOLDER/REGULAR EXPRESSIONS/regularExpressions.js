6e37f4d8bd7b6b10f37570ed31db260fc55a3bb0
CJMerriman <148406377+CJMerriman@users.noreply.github.com>
2024-03-12 22:30:05 -0400

    New notes

--- PRACTICE FOLDER/REGULAR EXPRESSIONS/regularExpressions.js
+++ PRACTICE FOLDER/REGULAR EXPRESSIONS/regularExpressions.js
@@ -1,11 +1,11 @@
-///Example look for all the capital I's
-const txt = "I love coding, I hope to become the greastest coder there, I feel like I've the best."
-const pattern = /\b[A-Z]\b/g
-const matches = txt.match(pattern)
+// ///Example look for all the capital I's
+// const txt = "I love coding, I hope to become the greastest coder there, I feel like I've the best."
+// const pattern = /\b[A-Z]\b/g
+// const matches = txt.match(pattern)
 
-console.log(matches)
+// console.log(matches)
 
-//Create a regular expression, that searches for the word regular, make it case insensitive
-//and search globally
-const txt = "Regular expressions are not hard, they are just regular REGULAR"
-const pattern = /regular/g
+// //Create a regular expression, that searches for the word regular, make it case insensitive
+// //and search globally
+// const txt = "Regular expressions are not hard, they are just regular REGULAR"
+// const pattern = /regular/g
