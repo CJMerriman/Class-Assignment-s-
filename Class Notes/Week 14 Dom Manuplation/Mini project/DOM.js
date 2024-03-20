f1ad799e7d0715e0f8868492d7839e2ead8d4401
CJMerriman <148406377+CJMerriman@users.noreply.github.com>
2024-03-19 20:54:14 -0400

    Mini project

--- Class Notes/Week 14 Dom Manuplation/Mini project/DOM.js
+++ Class Notes/Week 14 Dom Manuplation/Mini project/DOM.js
@@ -1,22 +1,22 @@
 // Create a div container on HTML document and create 100 to 100 numbers dynamically and append to the container div.
 function isPrime(num) {
-    if (num <= 1) return false;
-    if (num <= 3) return true;
-    if (num % 2 === 0 || num % 3 === 0) return false;
+    if(num <= 1) return false;
+    if(num <= 3) return true;
+    if(num % 2 === 0 || num % 3 === 0) return false;
     let i = 5;
-    while (i * i <= num) {
-        if (num % i === 0 || num % (i + 2) === 0) return false;
-        i += 5;
+    while(i * i <= num) {
+        if (num % i === 0 || num %(i + 2) === 0) return false;
+        i += 6;
     }
     return true;
 }
-function generatNumber(){
-    let container=document.getElementsByClassName(".container")
-for(let i=0; i<100; i++){
-    div=document.createElement("div")
-    div.textContent=`${i+1}`
-    div.className="number"
-    if(i%2===0){
+function generateNumber(){
+  let container=document.querySelector(`.container`)
+for(let i = 1; i <= 100; i++) {
+    div = document.createElement("div")
+    div.textContent=`${i}`
+    div.className = "number"
+    if(i % 2 === 0){
         div.style.backgroundColor="green"
       }else if(isPrime(i)){
         div.style.backgroundColor="red"
@@ -27,7 +27,7 @@ for(let i=0; i<100; i++){
       console.log(div)
     }
 }
-generatNumber
+generateNumber();
 
 // Even numbers background is green
 
