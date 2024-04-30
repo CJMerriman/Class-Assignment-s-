acc48291b8654fd4bee3993ebb8e6ae2bf50a36b
CJMerriman <148406377+CJMerriman@users.noreply.github.com>
2024-04-30 19:53:42 -0400

    all new work

---
+++ FrontEnd Libraries/Class Notes/React/jsx/src/index.js
@@ -0,0 +1,66 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// const headerStyle = (
+//     backgroundColor:"yellow"
+// )
+
+// const jsxElement = <h1>Welcome to day 1 of React</h1>
+// const rootElement=document.getElementById("root")
+
+// const header=(
+//     <header style={headerStyle}>
+//         <h1>Welcome to React</h1>
+//         <h2>This is the first day of React</h2>
+//         <p>React is JavaScript libarary that is created by facebook.</p>
+//     </header>
+// )
+
+// const mainStyle = {
+//     backgroundColor: "red"
+// }
+// const techs=["html", "Css", "JS"]
+// const techsFormated= tech.map((tech)=><li></li>{tech}</li>)
+// const main = (
+//     <main>
+//     <div style={mainStyle}>
+//         <p>The prerequsties of React is that you know the Basic coding languges listed below. </p>
+//         <ul>
+//         {techsFormted}
+        
+//         </ul>
+//     </div>
+// </main>
+// )
+
+// cont footer=(
+//     <div>
+//         <span></span>       
+//     </div>
+// )
+// const app=(
+//     <div>
+//         {header}
+//         {main}
+//         {footer}
+//     </div>
+// )
+
+// //ReactDOM is the library, render() is a method that comes from the ReactDOM library. It take two prameter which are what you are rendering and where you are rendering it too. 
+// ReactDOM.render(app, rootElement)
+//ReactDOM.render(what is being rendered, where it is being rendered)
+
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
